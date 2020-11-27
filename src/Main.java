5d2f9526af771ce82360e8a9a01ad008382038a1
Анарбек-хаб <vodka.gus@mail.ru>
2020-11-27 16:37:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Boss boss = new Boss(200, 50);
+
+        Weapon weapon = new Weapon("automat", "ak47");
+
+        boss.setWeapon(weapon);
+
+        System.out.println("BossHealth = " + boss.getHeathHB() +
+                " " + "BossDamage = " + boss.getDamageHB() +
+                " " + "BossWeapon - " + boss.getWeapon().getNameOfWeapon() +
+                " " + boss.getWeapon().getTypeOfWeapon());
+    }
+}
